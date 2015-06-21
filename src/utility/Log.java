c43ccdf5b44cbb64fb7159149ae533eec3efa593
Mauro <mauro.fustinoni@gmail.com>
2015-06-21 12:51:15 +0200

    0004 - Log utility improved

--- src/utility/Log.java
+++ src/utility/Log.java
@@ -4,12 +4,68 @@ import java.io.*;
 
 public class Log {
 
-	public static void write(String log) throws IOException {
-		File logFile = new File(
+	static File logFile;
+	static BufferedWriter logWriter;
+
+	/**
+	 * Create and initialize the log File
+	 */
+	public static void openLog() {
+		logFile = new File(
 				"C:\\Users\\Mauro\\Documents\\Paradox Interactive\\Crusader Kings II\\mod\\CK2-Randomizer\\log.txt");
-		BufferedWriter writer = new BufferedWriter(new FileWriter(logFile));
-		writer.append(log + "\n");
-		writer.close();
-		return;
+		try {
+			logWriter = new BufferedWriter(new FileWriter(logFile));
+		} catch (IOException e) {
+			// This error cannot be logged because we have no log file! xD
+			// Execution will fail
+			e.printStackTrace();
+		}
+		Log.write("Randomizer Execution - Log File");
+		Log.write("-------------------------------\n");
+		Log.write("Randomizer Execution - START");
+	}
+
+	/**
+	 * Close the log file
+	 */
+	public static void closeLog() {
+		try {
+			Log.write("Randomizer Execution - FINISH");
+			logWriter.close();
+		} catch (IOException e) {
+			// This error cannot be logged because we have problem on log file!
+			// Execution will fail
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Write simple row with no information
+	 * @param log - row to be written on log file
+	 */
+	public static void write(String log) {
+		try {
+			logWriter.append(log + "\n");
+		} catch (IOException e) {
+			// This error cannot be logged because we have problem on log file!
+			// Execution will fail
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Write an Info Log
+	 * @param log - row to be written on log file
+	 */
+	public static void info(String log) {
+		Log.write("INFO: " + log);
+	}
+	
+	/**
+	 * Write an Error Log
+	 * @param log - row to be written on log file
+	 */
+	public static void error(String log) {
+		Log.write("ERROR: " + log);
 	}
 }
